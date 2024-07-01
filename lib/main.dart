b60e8239a45f86ce0c50e4d3974316ae33b94f0c
Jason Sejik Lee <gggabikr@gmail.com>
2024-07-01 12:55:03 -0700

    First commit for Pomodoro app

--- lib/main.dart
+++ lib/main.dart
@@ -4,71 +4,26 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  int counter = 0;
-  List<int> numbers = [];
-
-  void onClicked() {
-    setState(() {
-      numbers.add(numbers.length + 1);
-      counter += 1;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
-            fontSize: 50,
+          headlineLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
       home: const Scaffold(
-        backgroundColor: Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MylargeTitle(),
-            ],
-          ),
-        ),
+        body: Center(),
       ),
-    );
-  }
-}
-
-class MylargeTitle extends StatefulWidget {
-  const MylargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MylargeTitle> createState() => _MylargeTitleState();
-}
-
-class _MylargeTitleState extends State<MylargeTitle> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text(
-      'MyLargeTitle',
-      style: TextStyle(fontSize: 40),
     );
   }
 }
